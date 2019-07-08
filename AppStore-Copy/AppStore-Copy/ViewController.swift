b17dd0a3529a945d3fc773c02099764017002124
Kirill Ivanov <ivanofff.k@gmail.com>
2019-07-09 01:10:38 +0300

    Started

---
+++ AppStore-Copy/AppStore-Copy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStore-Copy
+//
+//  Created by Кирилл Иванов on 09/07/2019.
+//  Copyright © 2019 Kirill Ivanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
