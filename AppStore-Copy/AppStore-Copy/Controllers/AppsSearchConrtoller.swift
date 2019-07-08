77c92a9d4558aee7bd6b115a42ffaa2ae07fa1df
Kirill Ivanov <ivanofff.k@gmail.com>
2019-07-09 01:56:09 +0300

    Created UICollectionViewController

---
+++ AppStore-Copy/AppStore-Copy/Controllers/AppsSearchConrtoller.swift
@@ -0,0 +1,24 @@
+//
+//  AppsSearchConrtoller.swift
+//  AppStore-Copy
+//
+//  Created by Кирилл Иванов on 09/07/2019.
+//  Copyright © 2019 Kirill Ivanoff. All rights reserved.
+//
+
+import UIKit
+
+class AppsSearchController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ AppStore-Copy/AppStore-Copy/Controllers/BaseTabController.swift
@@ -0,0 +1,31 @@
+//
+//  BaseTabController.swift
+//  AppStore-Copy
+//
+//  Created by Кирилл Иванов on 09/07/2019.
+//  Copyright © 2019 Kirill Ivanoff. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: AppsSearchController(), title: "Search", imageName:  "search")
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        return navController
+    }
+}
